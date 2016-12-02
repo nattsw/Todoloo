8db2543aa5018744202d08ce40d1fe8fb3795385
Natalie Tay <natalie.tay@gmail.com>
2016-12-02 11:55:37 +0800

    Initial Commit

---
+++ Todoloo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoloo
+//
+//  Created by Natalie Tay on 2/12/16.
+//  Copyright © 2016 Natalie Tay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
